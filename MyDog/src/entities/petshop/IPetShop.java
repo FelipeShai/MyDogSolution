5044aa2e5b4cc1885308a664f0590ada0954e84d
Felipe Shai <88523804+FelipeShai@users.noreply.github.com>
2023-07-14 01:24:47 -0300

    Delete MyDog directory

--- MyDog/src/entities/petshop/IPetShop.java
+++
@@ -1,8 +0,0 @@
-package entities.petshop;
-
-import java.time.DayOfWeek;
-
-public interface IPetShop {
-
-	public double calculateTotalPrice(DayOfWeek dayOfWeek, int smallDogsQuantity, int bigDogsQuantity);
-}
